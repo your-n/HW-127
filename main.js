4ff13eb83700cf792dd3dd165dcc5d7f3d99020a
your-n <87567750+your-n@users.noreply.github.com>
2022-04-29 17:13:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+song= "";
+
+function play() {
+song.play();
+}
+
+
+function setup() {
+canvas= createCanvas(600, 500);
+canvas.center();
+
+video= createCapture(VIDEO);
+video.hide();
+
+}
+
+function draw() {
+image(video, 0, 0, 600, 500);
+}
+
+function preload(){
+song = loadSound("Lovely.mp3");
+}
